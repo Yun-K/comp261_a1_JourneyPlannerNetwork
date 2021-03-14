850f6cc1ddab0f92e394cb09bddcf572e938a220
Yun Zhou <YunZhou0612@qq.com>
2021-03-14 16:06:32 +0800

    Update the Main class

--- comp261_a1/src/main/Main.java
+++ comp261_a1/src/main/Main.java
@@ -12,14 +12,24 @@ import myCode.Window;
 public class Main extends Window {
 
     /**
+     * A constructor. It construct a new instance of Main.
+     *
+     */
+    public Main() {
+        // TODO Auto-generated constructor stub
+        super();
+    }
+
+    /**
      * Description: <br/>
-     * The entrances of my network.
+     * The entrances of my application.
      * 
      * @author Yun Zhou
      * @param args
      *            arguments
      */
     public static void main(String[] args) {
+
         new Main();
     }
 
